1e709082a52497aae38d13a9076f76cee957a5b7
ub-victor <victoireushindi371@gmail.com>
2026-07-18 23:56:24 +0200

    Initial commit

--- student-registration-system/src/main/java/com/auca/student_registration_system/StudentRegistrationSystemApplication.java
+++ student-registration-system/src/main/java/com/auca/student_registration_system/StudentRegistrationSystemApplication.java
@@ -1,7 +1,6 @@
 package com.auca.student_registration_system;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StudentRegistrationSystemApplication {
