bc6474f1ffec5410a1def66f6d63880c3f1e190b
ub-victor <victoireushindi371@gmail.com>
2026-07-18 23:51:56 +0200

    Git ignore

---
+++ student-registration-system/src/main/java/com/auca/student_registration_system/StudentRegistrationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.auca.student_registration_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentRegistrationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentRegistrationSystemApplication.class, args);
+	}
+
+}
---
+++ student-registration-system/src/test/java/com/auca/student_registration_system/StudentRegistrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auca.student_registration_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentRegistrationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
